d7c529c10585365386a460c62ec291e6a1e3217d
daniel ouattara <daniel.ouattara@gmx.com>
2021-05-01 20:53:05 +0200

    Router, Link ,Route & http-common, via axios

--- frontend-react-redux-crud/src/App.js
+++ frontend-react-redux-crud/src/App.js
@@ -1,23 +1,50 @@
+import React, { Component } from 'react'
+import {BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import AddTutorial from "./components/add-tutorial.component.js";
+import Tutorial from "./components/tutorial.component.js";
+import TutorialsList from "./components/tutorials-list.component.js";
 
+
+class App extends Component {
+  render() { 
+    return (  
+      <Router>
+
+        <nav className="navbar navbar-expand navbar-dark bg-dark">
+
+          <Link to={"/tutorials"} className="navbar-brand" >
+            Tutorial App
+          </Link>
+
+          <div className="navbar-nav mr-auto">
+
+            <li className="nav-item">
+              <Link to={"/tutorials"} className="nav-link">Tutorials</Link>
+            </li>
+
+            <li className="nav-item">
+              <Link className="nav-link"> Add Tutorial</Link>
+            </li>
+
+          </div>
+
+        </nav>
+
+        <div className="container mt-3">
+          <Switch>
+            <Route exact path={["/", "/tutorials"]} component={TutorialsList}/>
+            <Route exact path="/add" component={AddTutorial}/>
+            <Route path="/tutorials/:id" component={Tutorial}/>
+          </Switch>
+        </div>
+        
+      </Router>
+    );
+  }
+}
+ 
 export default App;
+
